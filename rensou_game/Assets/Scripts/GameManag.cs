da80934b177e76e3abc9541baa78f955604b5213
Rensougame <shinyai0413@gmail.com>
2017-07-20 16:55:57 +0900

    シーン移行関数とメイン画面のデータ管理

---
+++ rensou_game/Assets/Scripts/GameManag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// メイン画面での各種データを扱う
+/// 作成者：下鳥信也
+/// 作成日時：2017/07/20
+/// </summary>
+
+public class GameManag : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- rensou_game/Assets/Scripts/SceneManage.cs
+++ rensou_game/Assets/Scripts/SceneManage.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// シーン切り替えの関数
+/// 作成者：下鳥信也
+/// 作成日時：2017/07/19
+/// </summary>
+
 public class SceneManage : MonoBehaviour {
 
 	// Use this for initialization
@@ -15,8 +21,27 @@ public class SceneManage : MonoBehaviour {
 		
 	}
 
-    void Main_Load()
+    //スタート画面に移行
+    public void Start_Load()
+    {
+        SceneManager.LoadScene("Start_Scene");
+    }
+
+    //メイン画面に移行
+    public void Main_Load()
     {
         SceneManager.LoadScene("Main_Scene");
+    }
+
+    //リザルト画面に移行
+    public void Result_Load()
+    {
+        SceneManager.LoadScene("Result_Scene");
+    }
+
+    //ランキング画面に移行
+    public void Ranking_Load()
+    {
+        SceneManager.LoadScene("Ranking_Scene");
     }
 }
