310c9b58f69a6d00a5af25759877510e7b4fbbca
Rensougame <shinyai0413@gmail.com>
2017-09-11 13:45:37 +0900

    操作修正、エフェクト

--- rensou_game/Assets/Scripts/Manager/GameManag.cs
+++ rensou_game/Assets/Scripts/Manager/GameManag.cs
@@ -13,9 +13,9 @@ using UnityEngine.UI;
 public class GameManag : MonoBehaviour {
 
     //現在の問題数
-    public int problem_num;
+    public int problem_num = 1;
     //最大問題数
-    public int problem_num_max;
+    public int problem_num_max = 10;
     //正解した問題数
     public int passed_problem_num;
     //ヒントを追加した数
@@ -32,6 +32,8 @@ public class GameManag : MonoBehaviour {
     public Image[] m_hintimage;
     //出ているヒントの数
     public int Hint_num = 2;
+    //最初に出ているヒントの数
+    public int Hint_num_first = 2;
     //ヒント最大数
     int Hint_num_max = 5;
     //ヒントimageのパス
@@ -46,7 +48,8 @@ public class GameManag : MonoBehaviour {
     public Image endImage;
     //背景Image
     public Image backImage;
-
+    //Image表示時間
+    public float activeImage;
 
     //excelを読み込んだデータ
     public Sheet1 m_Book;
@@ -60,8 +63,17 @@ public class GameManag : MonoBehaviour {
     public Text skip_text;
 
 
+    //ランダム重複検査用
+    bool[] rand_duplication;
+
+    //パーティクル(紙吹雪)
+    public ParticleSystem m_confetti;
+
+
     // Use this for initialization
     void Start () {
+        rand_duplication = new bool[m_Book.param.Count];
+        ID_random();
         m_inputField.ActivateInputField();
         startup_hint(1, Hint_num);
         correctImage.enabled = false;
@@ -122,6 +134,29 @@ public class GameManag : MonoBehaviour {
         }
     }
 
+    //エンターキーを押した場合、文字判定をする
+    void emter_push()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            letter_decision();
+        }
+    }
+
+    //問題のランダム
+    void ID_random()
+    {
+        ID = Random.Range(0, m_Book.param.Count);
+        if (rand_duplication[ID] == true)
+        {
+            ID_random();
+        }
+        else
+        {
+            rand_duplication[ID] = true;
+        }
+    }
+
     //正解・不正解後の処理
     void clear()
     {
@@ -129,11 +164,20 @@ public class GameManag : MonoBehaviour {
         incorrectImage.enabled = false;
         backImage.enabled = false;
 
+        m_confetti.Clear();
+        m_confetti.Stop();
+
         problem_num++;
-        ID++;
+        ID_random();
+        Hint_num = Hint_num_first;
 
         hint_clear();
         m_inputField.text = "";
+
+        if (problem_num >= m_Book.param.Count)
+        {
+            skip_text.text = "リタイア";
+        }
 
         startup_hint(1, Hint_num);
         m_skiptext_change.text_change();
@@ -142,13 +186,17 @@ public class GameManag : MonoBehaviour {
         m_inputField.ActivateInputField();
     }
 
-    //エンターキーを押した場合、文字判定をする
-    void emter_push()
+    //全問終了及びリタイア時の処理
+    void game_end()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            letter_decision();
-        }
+        correctImage.enabled = false;
+        incorrectImage.enabled = false;
+
+        m_confetti.Clear();
+        m_confetti.Stop();
+
+        backImage.enabled = true;
+        endImage.enabled = true;
     }
 
     //**********************************************
@@ -170,7 +218,7 @@ public class GameManag : MonoBehaviour {
     public void skip_feature()
     {
         int skip_change = 0;
-        if (skip_num > 0)
+        if (skip_num > 0 && problem_num < m_Book.param.Count)
         {
             skip_num--;
             if (skip_num == skip_change) skip_text.text = "リタイア";
@@ -179,8 +227,7 @@ public class GameManag : MonoBehaviour {
         }
         else
         {
-            endImage.enabled = true;
-            backImage.enabled = true;
+            game_end();
         }
     }
 
@@ -192,13 +239,31 @@ public class GameManag : MonoBehaviour {
             correctImage.enabled = true;
             backImage.enabled = true;
             passed_problem_num++;
-            Invoke("clear", 3.5f);
+
+            m_confetti.Play();
+
+            if (problem_num >= m_Book.param.Count)
+            {
+                Invoke("game_end", activeImage);
+            }
+            else
+            {
+                Invoke("clear", activeImage);
+            }
         }
         else
         {
             incorrectImage.enabled = true;
             backImage.enabled = true;
-            Invoke("clear", 3.5f);
+
+            if (problem_num >= m_Book.param.Count)
+            {
+                Invoke("game_end", activeImage);
+            }
+            else
+            {
+                Invoke("clear", activeImage);
+            }
         }
     }
 }
